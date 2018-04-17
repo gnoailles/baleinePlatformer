6467679ac127e8a7406dcbde80a8357407569700
Unknown <e.haquin@student.isartdigital.com>
2018-04-17 11:23:11 -0400

    Fixing jump and movements

--- BaleinePlatformer/Assets/Scripts/Player/PlayerMovements.cs
+++ BaleinePlatformer/Assets/Scripts/Player/PlayerMovements.cs
@@ -7,55 +7,49 @@ public class PlayerMovements : MonoBehaviour {
     [SerializeField] private string xAxis = "Horizontal";
     [SerializeField] private string jumpAxis = "Jump";
     [SerializeField] private float speed = 5.0f;
-    [SerializeField] private float jumpAcceleration = 5.0f;
-    [SerializeField] private float jumpSpeed = 15.0f;
+    [SerializeField] private float jumpHeight = 5.0f;
 
-    private CharacterController characterController;
+    private Rigidbody playerRigidbody;
+    private Vector3 gravity = Vector3.zero;
     private Vector3 jumpDirection = Vector3.zero;
     private float X = 0.0f;
     private float Y = 0.0f;
+    private float jumpForce = 0.0f;
 
     private void Start()
     {        
-        characterController = gameObject.GetComponent<CharacterController>();
-        characterController.detectCollisions = false;
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+        jumpForce = Mathf.Sqrt((jumpHeight) * 2f * gravity.magnitude) * playerRigidbody.mass;
+        gravity = Physics.gravity;
     }
 
     void Update ()
     {
-        X = Input.GetAxis(xAxis);
+        X = Input.GetAxisRaw(xAxis);
         Y = Input.GetAxisRaw(jumpAxis);
 	}
 
     void Jump()
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
+    {    
+        if (Input.GetButton(jumpAxis) && Physics.Raycast(transform.position, Vector3.down, 0.5f) )
         {
-            jumpDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            jumpDirection = transform.TransformDirection(jumpDirection);
 
-            if (Input.GetButton("Jump"))
-            {
-                jumpDirection *= jumpAcceleration;
-                jumpDirection.y = jumpSpeed;
-            }
-            
+#if UNITY_EDITOR
+            jumpForce = Mathf.Sqrt((jumpHeight) * 2f * gravity.magnitude) * playerRigidbody.mass;
+#endif
+            playerRigidbody.AddForce(jumpForce * transform.up, ForceMode.Impulse);
+                    
         }
     }
-    void MovePlayer(float horiz, float vert)
-    { 
-        Vector3 moveDirection = transform.right * horiz * speed;
-        moveDirection += transform.forward * vert * speed;
-        characterController.SimpleMove(moveDirection);
-        
+    void MovePlayer(float horiz)
+    {
+        Vector3 moveDirection = playerRigidbody.velocity;
+        moveDirection.x = horiz * speed;
+        playerRigidbody.velocity = moveDirection ;       
     }
     void FixedUpdate()
     {
-        if (Y != 0 || X != 0)
-            MovePlayer(X, Y);
-
-        Jump();
-        jumpDirection.y -= 9.81f * Time.deltaTime;
-        characterController.Move(jumpDirection * Time.deltaTime);
+       MovePlayer(X);
+       Jump();        
     }
 }
